2e6bb59f04fedee1507d5c8bdf7494344ce40ea6
Yl <Yl@naver.com>
2021-10-01 19:36:46 +0900

    Initial Commit

---
+++ NewlyCoinedWordAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewlyCoinedWordAssignment
+//
+//  Created by Yundong Lee on 2021/10/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
