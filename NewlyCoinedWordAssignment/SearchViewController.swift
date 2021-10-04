68d3d4c36a2679e5ca9fc7127227bb3e6e2ddcd9
Yl <Yl@naver.com>
2021-10-04 23:41:22 +0900

    10.04 finish

--- NewlyCoinedWordAssignment/SearchViewController.swift
+++ NewlyCoinedWordAssignment/SearchViewController.swift
@@ -11,10 +11,20 @@ class SearchViewController: UIViewController {
 
     @IBOutlet weak var searchTextField: UITextField!
     
-    @IBOutlet weak var firstButton: UIButton!
-    @IBOutlet weak var secondButton: UIButton!
-    @IBOutlet weak var thirdButton: UIButton!
-    @IBOutlet weak var fourthButton: UIButton!
+    @IBOutlet var worButtons: [UIButton]!{
+        didSet{
+            for index in worButtons.indices{
+                if worButtons[index].isHidden{
+                    
+                }else{
+                    worButtons[index].layer.borderColor = UIColor.black.cgColor
+                    worButtons[index].layer.borderWidth = 1
+                    
+                }
+            }
+        }
+    }
+    
     
     @IBOutlet weak var resultLabel: UILabel!
     
@@ -23,16 +33,33 @@ class SearchViewController: UIViewController {
         "존멋" : "존나멋있어",
         "갑뿐싸" : "갑자기 분위기 싸하다",
         "존예" : "존나 예뻐"
-        
     ]
     
-    var currentWordArray : [String] = []
+    var currentWordArray : [String] = []{
+        didSet{
+            for index in currentWordArray.indices{
+                worButtons[index].isHidden = false
+                worButtons[index].setTitle(currentWordArray[index], for: .normal)
+            }
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchTextField.layer.borderColor = UIColor.black.cgColor
+        searchTextField.layer.borderWidth = 3
+        
+        for index in worButtons.indices{
+            worButtons[index].isHidden = true
+        }
+        
     }
 
-
+    @IBAction func wordButtonClicked(_ sender: UIButton) {
+        searchTextField.text = sender.currentTitle
+        
+    }
+    
     @IBAction func tapOutsideScreen(_ sender: UITapGestureRecognizer) {
         view.endEditing(true)
     }
@@ -43,7 +70,6 @@ class SearchViewController: UIViewController {
     }
     
     @IBAction func searchButtonPressed(_ sender: UIButton) {
-        
         searchPart()
     }
     
@@ -51,16 +77,14 @@ class SearchViewController: UIViewController {
         if let searchedWord = searchTextField.text, let meaning = wordDict[searchedWord]{
             resultLabel.text = meaning
             if currentWordArray.count < 4{
-                currentWordArray.append(contentsOf: searchedWord)
+                currentWordArray.append(searchedWord)
             }else{
                 currentWordArray.remove(at: 0)
-                currentWordArray.append(contentsOf: searchedWord)
+                currentWordArray.append(searchedWord)
             }
         }else{
             resultLabel.text = "없는 단어입니다."
         }
-        
-        
         
         view.endEditing(true)
     }
